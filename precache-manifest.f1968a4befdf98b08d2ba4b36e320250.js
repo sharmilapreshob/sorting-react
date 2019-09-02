e7d8d35ae1ceec042d5c6ae81743731dfc45c6cd
sharmilapreshob <sharmila.preshob@gmail.com>
2019-09-02 16:51:27 +0400

    Updates

---
+++ precache-manifest.f1968a4befdf98b08d2ba4b36e320250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886ec90d4a8d30478da086c858050a41",
+    "url": "/yilu/index.html"
+  },
+  {
+    "revision": "1db1d1eb2cf0b0890b28",
+    "url": "/yilu/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b3abdf7479129b3c2832",
+    "url": "/yilu/static/css/main.d8da9922.chunk.css"
+  },
+  {
+    "revision": "1db1d1eb2cf0b0890b28",
+    "url": "/yilu/static/js/2.d95151f7.chunk.js"
+  },
+  {
+    "revision": "b3abdf7479129b3c2832",
+    "url": "/yilu/static/js/main.7525d0d0.chunk.js"
+  },
+  {
+    "revision": "b16c4d164c8530edbdff",
+    "url": "/yilu/static/js/runtime~main.4d3b2cc9.js"
+  }
+]);
